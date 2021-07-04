a7c3e6005d95d3a2db305103b613827ddfc39992
Jan Surdziel <surdzieljan1994@gmail.com>
2021-07-04 12:41:09 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hangman_Jan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
